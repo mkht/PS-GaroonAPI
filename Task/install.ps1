d576dc25d467b8ddcd5d20451039717157c6c115
iidi16 <iidi16@d-staff.dokkyo.ac.jp>
2017-09-12 10:17:25 +0900

    タスク失敗時の例外処理追加

--- Task/install.ps1
+++ Task/install.ps1
@@ -2,12 +2,15 @@
 $Dst = "$env:SystemDrive\Program Files\WindowsPowerShell\Modules"
 $ModuleName = "PS-GaroonAPI"
 
-if(Get-ChildItem $Src -Filter "*.psm1"){
-    if(Test-Path($Dst)){
-        New-Item -Path (Join-Path $Dst $ModuleName) -Value $Src -ItemType SymbolicLink -Force -Verbose
-        Write-Host 'Success'
-        return
+try {
+    if (Get-ChildItem $Src -Filter "*.psm1") {
+        if (Test-Path($Dst)) {
+            New-Item -Path (Join-Path $Dst $ModuleName) -Value $Src -ItemType SymbolicLink -Force -Verbose -ErrorAction Stop
+            Write-Host 'Success'
+            return
+        }
     }
 }
-
-Write-Warning "Fail"
+catch {
+    Write-Warning "Fail"
+}
